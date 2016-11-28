6159d87b4c8467f8d123cbacf18226af367c7ffb
Chris Jones <cj@hanscj.com>
2016-11-28 09:36:00 -0500

    Updated docs

---
+++ docs/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lsiotemplate"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='lsiotemplate/enum.OutputFormat.html' title='lsiotemplate::OutputFormat'>OutputFormat</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='lsiotemplate/struct.ErrorOutput.html' title='lsiotemplate::ErrorOutput'>ErrorOutput</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='lsiotemplate/struct.Output.html' title='lsiotemplate::Output'>Output</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='lsiotemplate/enum.Commands.html' title='lsiotemplate::Commands'>Commands</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='lsiotemplate/enum.TemplateTypes.html' title='lsiotemplate::TemplateTypes'>TemplateTypes</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='lsiotemplate/enum.DataTypes.html' title='lsiotemplate::DataTypes'>DataTypes</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
